af9ce57a69ed961006b6791311bc52ebbe98b017
Georg Rutishauser <georgr@iis.ee.ethz.ch>
2022-10-18 10:04:54 +0200

    remove redundant commands from docker setup scripts

--- docker_utils/docker_gap_sdk.sh
+++ docker_utils/docker_gap_sdk.sh
@@ -1,11 +1,5 @@
 #! /usr/bin/env bash
 
-if [ ! $# -eq 0 ]; then
-    CONTAINER_NAME=$1
-else
-    CONTAINER_NAME=dory_docker
-    echo "No container name supplied; running container 'dory_docker'"
-fi
 SCRIPT_PATH=`realpath $BASH_SOURCE`
 SCRIPT_DIR=`dirname $SCRIPT_PATH`
 DORY_HOME=`dirname $SCRIPT_DIR`
