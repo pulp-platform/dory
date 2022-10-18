633ee5228a49728d299cdb86423f31b2e6ec5c15
Georg Rutishauser <georgr@iis.ee.ethz.ch>
2022-10-18 08:56:19 +0200

    docker: fix path to gap_sdk configs

--- docker_utils/docker_gap_sdk.sh
+++ docker_utils/docker_gap_sdk.sh
@@ -12,6 +12,6 @@ DORY_HOME=`dirname $SCRIPT_DIR`
 unset PULP_RISCV_GCC_TOOLCHAIN
 
 source /dory_env/bin/activate
-source /gap_riscv_toolchain_ubuntu_18/configs/gapuino_v3.sh
+source /gap_riscv_toolchain_ubuntu_18/gap_sdk/configs/gapuino_v3.sh
 
 find $DORY_HOME  -path "*GAP8*HW_description.json" -exec sed -E -i 's/("name" *: *)"pulp-sdk"/\1"gap_sdk"/' {} +
