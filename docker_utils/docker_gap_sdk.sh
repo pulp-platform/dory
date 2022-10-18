a9462fad1b3c4444a4ff7b203a2661c4a75fcebb
Georg Rutishauser <georgr@iis.ee.ethz.ch>
2022-10-18 09:09:13 +0200

    final fixes to docker util scripts

--- docker_utils/docker_gap_sdk.sh
+++ docker_utils/docker_gap_sdk.sh
@@ -1,6 +1,6 @@
 #! /usr/bin/env bash
 
-if [ ! $# -eq 0]; then
+if [ ! $# -eq 0 ]; then
     CONTAINER_NAME=$1
 else
     CONTAINER_NAME=dory_docker
--- docker_utils/docker_pulp_sdk.sh
+++ docker_utils/docker_pulp_sdk.sh
@@ -1,8 +1,19 @@
 #! /usr/bin/env bash
 
+
+if [ ! $# -eq 0 ]; then
+    CONTAINER_NAME=$1
+else
+    CONTAINER_NAME=dory_docker
+    echo "No container name supplied; running container 'dory_docker'"
+fi
+SCRIPT_PATH=`realpath $BASH_SOURCE`
+SCRIPT_DIR=`dirname $SCRIPT_PATH`
+DORY_HOME=`dirname $SCRIPT_DIR`
+
 source /dory_env/bin/activate
 source /pulp-sdk/configs/pulp-open-nn.sh
 export PULP_RISCV_GCC_TOOLCHAIN=/riscv-nn-toolchain/
 
-find /dory_checkout -path "*GAP8*HW_description.json" -exec sed -E -i 's/("name" *: *)"gap_sdk"/\1"pulp-sdk"/' {} +
+find $DORY_HOME -path "*GAP8*HW_description.json" -exec sed -E -i 's/("name" *: *)"gap_sdk"/\1"pulp-sdk"/' {} +
 
