3e6dc7f651e8c87af1f4684f70b6a4d538c5c489
Luka Macan <luka.macan@unibo.it>
2023-03-06 11:14:10 +0100

    Generalize functions and remove defines
    You have to manually add the checksum or print in your
    code if you want debug it. Makes the original code look
    clearer. There is probably some middle-ground.
    
    Signed-off-by: Luka Macan <luka.macan@unibo.it>

--- Hardware-targets/nnx/Utils_files/layer_debug.h
+++ Hardware-targets/nnx/Utils_files/layer_debug.h
@@ -1,39 +1,46 @@
 #ifndef __LAYER_DEBUG_H__
 #define __LAYER_DEBUG_H__
 
-#ifdef DEBUG_TILE_CHECKSUM
-#define TILE_CHECKSUM_PRINT(tile, i_tile)                 \ 
-    do {                                                  \
-        uint8_t *ptr = (uint8_t *)tile.addr.output;       \
-        int sum = 0;                                      \
-        for (int i = 0; i < tile.output.height; i++)      \
-          for (int j = 0; j < tile.output.width; j++)     \
-            for (int k = 0; k < tile.output.channel; k++) \
-              sum += *ptr++;                              \
-        printf("[%d] Checksum: %d\n", i_tile, sum);       \
+#define FEATURE_MAP_CHECKSUM(addr, dims, i_tile)    \
+    do {                                            \
+        uint8_t *ptr = (uint8_t *)addr;             \
+        int sum = 0;                                \
+        for (int i = 0; i < dims.height; i++)       \
+          for (int j = 0; j < dims.width; j++)      \
+            for (int k = 0; k < dims.channel; k++)  \
+              sum += *ptr++;                        \
+        printf("[%d] Checksum: %d\n", i_tile, sum); \
     } while (0)
-#else
-#define TILE_CHECKSUM_PRINT(tile, i_tile)
-#endif
 
-#ifdef DEBUG_TILE_OUTPUT
-#define TILE_OUTPUT_PRINT(tile, i_tile)                     \ 
-    do {                                                    \
-        printf("[%d] Output:\n", i_tile);                   \
-        uint8_t *ptr = (uint8_t *)tile.addr.output;         \
-        for (int i = 0; i < tile.output.height; i++) {      \
-          for (int j = 0; j < tile.output.width; j++) {     \
-            for (int k = 0; k < tile.output.channel; k++) { \
-             printf("%d, ", *ptr++);                        \
-            }                                               \
-            printf("\n");                                   \
-          }                                                 \
-          printf("\n");                                     \
-        }                                                   \
-        printf("\n");                                       \
+#define DMA_CHECKSUM_PRINT(conf, i_tile)                     \
+    do {                                                     \
+        uint8_t *ptr = (uint8_t *)conf.loc;                  \
+        int sum = 0;                                         \
+        for (int i = 0; i < conf.number_of_2d_copies; i++)   \
+          for (int j = 0; j < conf.number_of_1d_copies; j++) \
+            for (int k = 0; k < conf.length_1d_copy; k++)    \
+              sum += *ptr++;                                 \
+        printf("[%d] Checksum: %d\n", i_tile, sum);          \
     } while (0)
-#else
-#define TILE_OUTPUT_PRINT(tile, i_tile)
-#endif
+
+#define FEATURE_MAP_PRINT(name, addr, dims, i_tile) \
+    do {                                            \
+        printf("[%d] " #name ":\n", i_tile);        \
+        uint8_t *ptr = (uint8_t *)addr;             \
+        for (int i = 0; i < dims.height; i++)       \
+          for (int j = 0; j < dims.width; j++)      \
+            for (int k = 0; k < dims.channel; k++)  \
+             printf("%d\n", *ptr++);                \
+    } while (0)
+
+#define DMA_FEATURE_MAP_PRINT(name, conf, i_tile)            \
+    do {                                                     \
+        printf("[%d] " #name ":\n", i_tile);                 \
+        uint8_t *ptr = (uint8_t *)conf.loc;                  \
+        for (int i = 0; i < conf.number_of_2d_copies; i++)   \
+          for (int j = 0; j < conf.number_of_1d_copies; j++) \
+            for (int k = 0; k < conf.length_1d_copy; k++)    \
+             printf("%d\n", *ptr++);                         \
+    } while (0)
 
 #endif  // __LAYER_DEBUG_H__
