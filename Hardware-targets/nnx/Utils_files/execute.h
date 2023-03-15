626bd92cc09b17d03ea11a251e323550cac9fe3a
Luka Macan <luka.macan@unibo.it>
2023-03-15 19:10:13 +0100

    Bump pulp-nnx
    Signed-off-by: Luka Macan <luka.macan@unibo.it>

--- Hardware-targets/nnx/Utils_files/execute.h
+++ Hardware-targets/nnx/Utils_files/execute.h
@@ -31,20 +31,20 @@ static void execute_prepare(Layer tile, nnx_task_t * const task) {
                          tile.output.width, tile.output.channel);
     nnx_conv_set_counters(task, tile.input.channel, tile.output.height, tile.output.width, tile.output.channel, tile.padding.bottom, tile.padding.right);
 
-    task->cfg.padding = (tile.padding.top << 28)
+    task->data.cfg.padding = (tile.padding.top << 28)
         | (tile.padding.right << 24)
         | (tile.padding.bottom << 20)
         | (tile.padding.left << 16)
         | 0;
 
-    task->infeat_ptr = padded_input_ptr(tile.addr.input, tile.padding.top,
+    task->data.infeat_ptr = padded_input_ptr(tile.addr.input, tile.padding.top,
                                         tile.padding.left, tile.input.width,
                                         tile.input.channel, 8);
-    task->weights_ptr = tile.addr.weights;
-    task->scale_ptr = tile.addr.scale;
-    task->scale_bias_ptr = tile.addr.bias;
-    task->scale_shift_ptr = 0;
-    task->outfeat_ptr = tile.addr.output;
+    task->data.weights_ptr = tile.addr.weights;
+    task->data.scale_ptr = tile.addr.scale;
+    task->data.scale_bias_ptr = tile.addr.bias;
+    task->data.scale_shift_ptr = 0;
+    task->data.outfeat_ptr = tile.addr.output;
 }
 
 static int execute_async(nnx_task_t task) {
@@ -64,19 +64,19 @@ static void execute_stride2x2_prepare(Layer tile, Kernel kernel, nnx_task_t * co
     tile.padding.bottom = (tile.input.height + tile.padding.top - kernel.shape.height) % stride == 0 ? 0 : tile.padding.bottom;
     tile.padding.right = (tile.input.width + tile.padding.left - kernel.shape.width) % stride == 0 ? 0 : tile.padding.right;
 
-    task->cfg.padding = (tile.padding.top << 28)
+    task->data.cfg.padding = (tile.padding.top << 28)
         | (tile.padding.right << 24)
         | (tile.padding.bottom << 20)
         | (tile.padding.left << 16)
         | 0;
 
-    task->infeat_ptr = padded_input_ptr(tile.addr.input, tile.padding.top,
+    task->data.infeat_ptr = padded_input_ptr(tile.addr.input, tile.padding.top,
                                         tile.padding.left, tile.input.width,
                                         tile.input.channel, 8);
-    task->weights_ptr = tile.addr.weights;
-    task->scale_ptr = tile.addr.scale;
-    task->scale_bias_ptr = tile.addr.bias;
-    task->outfeat_ptr = tile.addr.output;
+    task->data.weights_ptr = tile.addr.weights;
+    task->data.scale_ptr = tile.addr.scale;
+    task->data.scale_bias_ptr = tile.addr.bias;
+    task->data.outfeat_ptr = tile.addr.output;
 }
 
 static int execute_stride2x2_blocking(nnx_task_t task, Layer tile, Kernel kernel) {
@@ -90,14 +90,14 @@ static int execute_stride2x2_blocking(nnx_task_t task, Layer tile, Kernel kernel
     const int output_height_offset = tile.output.height % stride == 1 ? 1 : 0;
     const int output_width_offset  = tile.output.width  % stride == 1 ? 1 : 0;
 
-    const uint32_t input_base = task.infeat_ptr;
-    const uint32_t output_base = task.outfeat_ptr;
-    const uint32_t tile_padding = task.cfg.padding;
+    const uint32_t input_base = task.data.infeat_ptr;
+    const uint32_t output_base = task.data.outfeat_ptr;
+    const uint32_t tile_padding = task.data.cfg.padding;
 
     int i_job = 0;
     for (int i = 0; i < n_h; i++) {
         for (int j = 0; j < n_w; j++) {
-            task.infeat_ptr = dory_get_tile_3d(input_base,
+            task.data.infeat_ptr = dory_get_tile_3d(input_base,
                                                i, j, 0,                    /* index */
                                                3 + kernel.shape.height - 1, 3 + kernel.shape.width - 1, tile.input.channel,        /* size */
                                                tile.input.width, tile.input.channel, /* stride */
@@ -105,7 +105,7 @@ static int execute_stride2x2_blocking(nnx_task_t task, Layer tile, Kernel kernel
                                                i == 0 ? 0 : input_height_offset, j == 0 ? 0 : input_width_offset, 0, /* offset */
                                                8 /* data size */
                                                );
-            task.outfeat_ptr = dory_get_tile_3d(output_base,
+            task.data.outfeat_ptr = dory_get_tile_3d(output_base,
                                                 i, j, 0,                      /* index */
                                                 2, 2, tile.output.channel,         /* size */
                                                 tile.output.width, tile.output.channel, /* stride */
@@ -114,15 +114,15 @@ static int execute_stride2x2_blocking(nnx_task_t task, Layer tile, Kernel kernel
                                                 8 /* data size */
                                                 );
 
-            task.cfg.padding = get_padding(i, j, n_h, n_w, tile_padding);
+            task.data.cfg.padding = get_padding(i, j, n_h, n_w, tile_padding);
 
             last_job_id = nnx_acquire_blocking();
             if (i_job < 2) {
                 nnx_offload(&task);
             } else {
-                NE16_WRITE_IO_REG(NE16_REG_INFEAT_PTR, task.infeat_ptr);
-                NE16_WRITE_IO_REG(NE16_REG_OUTFEAT_PTR, task.outfeat_ptr);
-                NE16_WRITE_IO_REG(NE16_REG_PADDING, task.cfg.padding);
+                NE16_WRITE_IO_REG(NE16_REG_INFEAT_PTR, task.data.infeat_ptr);
+                NE16_WRITE_IO_REG(NE16_REG_OUTFEAT_PTR, task.data.outfeat_ptr);
+                NE16_WRITE_IO_REG(NE16_REG_PADDING, task.data.cfg.padding);
             }
             nnx_run_async();
 
