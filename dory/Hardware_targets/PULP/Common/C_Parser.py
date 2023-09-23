e4c37daa3798427d700ca713d2efade81e8e5cba
Luka Macan <luka.macan@unibo.it>
2023-09-23 10:25:16 +0000

    Make precision library decision same for the whole graph

--- dory/Hardware_targets/PULP/Common/C_Parser.py
+++ dory/Hardware_targets/PULP/Common/C_Parser.py
@@ -35,7 +35,7 @@ class C_Parser_PULP(Parser_HW_to_C):
         file_path = self.get_file_path()
         with open(os.path.join(file_path, "HW_description.json")) as f:
             HW_description = json.load(f)
-        self.precision_library = precision_library
+        self.precision_library = C_Parser_PULP._auto_precision_library(graph) if precision_library == "auto" else precision_library
         self.source_Constant_bits_library = config_file["BNRelu_bits"]
         self.config_file = config_file
         super().__init__(graph, os.path.join(config_file_dir, os.path.dirname(config_file["onnx_file"])), HW_description, verbose_level, perf_layer, "Makefile", app_directory, n_inputs)
@@ -46,27 +46,33 @@ class C_Parser_PULP(Parser_HW_to_C):
             db = 2
         self.double_buffering = db
 
+    @staticmethod
+    def _auto_precision_library(graph):
+        precision_library = "8bit"
+        for node in graph:
+            if "Addition" not in node.name and "Pool" not in node.name:
+                if node.get_parameter('output_activation_bits') < 8 or node.get_parameter('input_activation_bits') < 8 or node.get_parameter('weight_bits') < 8:
+                    precision_library = 'mixed-sw'
+            else:
+                if node.get_parameter('output_activation_bits') < 8 or node.get_parameter('input_activation_bits') < 8:
+                    precision_library = 'mixed-sw'
+        return precision_library
+
     def get_file_path(self):
         raise NotImplementedError("To be implemented by child class!")
 
-    def copy_backend_files(self, node):
-        if self.precision_library == 'auto':
-            self.precision_library = '8bit'
-            if "Addition" not in node.name and "Pool" not in node.name:
-                if node.get_parameter('output_activation_bits') < 8 or node.get_parameter('input_activation_bits') < 8 or node.get_parameter('weight_bits') < 8:
-                    self.precision_library = 'mixed-sw'
-            else:
-                if node.get_parameter('output_activation_bits') < 8 or node.get_parameter('input_activation_bits') < 8:
-                    self.precision_library = 'mixed-sw'
+    def node_backend_library(self, node):
+        return self.precision_library
 
-        if self.precision_library == "8bit":
+    def copy_backend_files(self, node, backend_library):
+        if backend_library == "8bit":
             backendKernelsAdapter = BackendKernelsAdapter.PulpNNAdapter("pulp-nn", node, self.source_Constant_bits_library)
-        elif self.precision_library == "mixed-sw":
+        elif backend_library == "mixed-sw":
             backendKernelsAdapter = BackendKernelsAdapter.PulpMixedAdapter("pulp-nn-mixed", node, self.source_Constant_bits_library, "sw")
-        elif self.precision_library == "mixed-hw":
+        elif backend_library == "mixed-hw":
             backendKernelsAdapter = BackendKernelsAdapter.PulpMixedAdapter("pulp-nn-mixed", node, self.source_Constant_bits_library, "hw")
         else:
-            raise ValueError(f"Unrecognised backend library: {self.precision_library}")
+            raise ValueError(f"Unrecognised backend library: {backend_library}")
 
         src_dir = os.path.join(self.app_directory, self.src_dir_rel)
         for file in backendKernelsAdapter.get_src_files():
@@ -82,7 +88,8 @@ class C_Parser_PULP(Parser_HW_to_C):
         out_dir = self.app_directory
         n_memory_levels = self.HW_description['memory']['levels']
         for i, node in enumerate(self.HWgraph):
-            self.copy_backend_files(node)
+            backend_library = self.node_backend_library(node)
+            self.copy_backend_files(node, backend_library)
 
             if n_memory_levels > 2 and (node.L3_input != 0 or (node.tiling_dimensions["L3"]["output_dimensions"] != node.tiling_dimensions["L2"]["output_dimensions"]) or (node.tiling_dimensions["L3"]["weights_dimensions"] != node.tiling_dimensions["L2"]["weights_dimensions"])):
                 Layer2D_writer.print_template_layer_L3(node, tmpl_dir, out_dir)
@@ -93,12 +100,12 @@ class C_Parser_PULP(Parser_HW_to_C):
                 node.name = node.name + "_L2"
                 padding = node.pads
                 node.pads = [0, padding[1], 0, padding[3]]
-                Layer2D_writer.print_template_layer(node, self.precision_library, tmpl_dir, out_dir, double_buffering=self.double_buffering)
+                Layer2D_writer.print_template_layer(node, backend_library, tmpl_dir, out_dir, double_buffering=self.double_buffering)
                 node.name = node.name[:-3]
                 if padding[0] > 0:
                     node.name = node.name + "_L2_p_t"
                     node.pads = [padding[0], padding[1], 0, padding[3]]
-                    Layer2D_writer.print_template_layer(node, self.precision_library, tmpl_dir, out_dir, double_buffering=self.double_buffering)
+                    Layer2D_writer.print_template_layer(node, backend_library, tmpl_dir, out_dir, double_buffering=self.double_buffering)
                     node.name = node.name[:-1] + "b"
                     node.pads = [0, padding[1], padding[2], padding[3]]
                     node.tiling_dimensions["L2"]["input_dimensions"][1] -= (padding[2] - ((node.tiling_dimensions["L3"]["input_dimensions"][1] + padding[0] + padding[2]) - (node.tiling_dimensions["L3"]["output_dimensions"][1]* node.strides[0] + node.kernel_shape[0] - node.strides[0])))
@@ -106,14 +113,14 @@ class C_Parser_PULP(Parser_HW_to_C):
                         node.tiling_dimensions["L1"]["input_dimensions"][1] = node.tiling_dimensions["L2"]["input_dimensions"][1]
                     if node.tiling_dimensions["L1"]["output_dimensions"][1] > node.tiling_dimensions["L2"]["output_dimensions"][1]:
                         node.tiling_dimensions["L1"]["output_dimensions"][1] = node.tiling_dimensions["L2"]["output_dimensions"][1]
-                    Layer2D_writer.print_template_layer(node, self.precision_library, tmpl_dir, out_dir, double_buffering=self.double_buffering)
+                    Layer2D_writer.print_template_layer(node, backend_library, tmpl_dir, out_dir, double_buffering=self.double_buffering)
                     node.name = node.name[:-7]
             else:
                 if node.tiling_dimensions["L2"]["input_dimensions"][2] == node.tiling_dimensions["L1"]["input_dimensions"][2]:
                     node.tiling_dimensions["L1"]["output_dimensions"][2] = int((node.tiling_dimensions["L1"]["input_dimensions"][2] + (node.pads[1] + node.pads[3]) - node.kernel_shape[1] + node.strides[1]) / node.strides[1])
                 if node.tiling_dimensions["L2"]["input_dimensions"][1] == node.tiling_dimensions["L1"]["input_dimensions"][1]:
                     node.tiling_dimensions["L1"]["output_dimensions"][1] = int((node.tiling_dimensions["L1"]["input_dimensions"][1] + (node.pads[0] + node.pads[2]) - node.kernel_shape[0] + node.strides[0]) / node.strides[0])
-                Layer2D_writer.print_template_layer(node, self.precision_library, tmpl_dir, out_dir, double_buffering=self.double_buffering)
+                Layer2D_writer.print_template_layer(node, backend_library, tmpl_dir, out_dir, double_buffering=self.double_buffering)
 
     def mapping_makefile(self):
         super(C_Parser_PULP, self).mapping_makefile()
