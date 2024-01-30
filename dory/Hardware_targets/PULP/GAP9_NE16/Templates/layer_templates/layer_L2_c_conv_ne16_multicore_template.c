7d3fad7ad01283f74db806b464d7baddd53eda60
Luka Macan <luka.macan@unibo.it>
2024-01-30 14:55:24 +0100

    Move ne16 init to network

--- dory/Hardware_targets/PULP/GAP9_NE16/Templates/layer_templates/layer_L2_c_conv_ne16_multicore_template.c
+++ dory/Hardware_targets/PULP/GAP9_NE16/Templates/layer_templates/layer_L2_c_conv_ne16_multicore_template.c
@@ -387,9 +387,6 @@ void ${func_name}(void *args) {
         ne16_task_set_weight_offset(&ne16_tasks[i], weightOffsetModeLayerWise, ${weight_offset});
     }
 
-    const ne16_pulp_conf_t ne16_pulp_conf = {.max_stall = 8};
-    ne16_nnx_init(ne16_pulp_get_dev(), &ne16_pulp_conf);
-
 
     // Fork
 
@@ -401,5 +398,4 @@ void ${func_name}(void *args) {
     monitor_term(monitor.input);
     monitor_term(monitor.output);
     monitor_term(monitor.store_conf);
-    ne16_nnx_term(ne16_pulp_get_dev());
 }
